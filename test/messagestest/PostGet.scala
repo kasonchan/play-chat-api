ecb027a46c249ca09b9bfef596d1ffdf59cff0de
KasonChan <kasonl.chan@gmail.com>
2015-06-16 10:28:43 -0500

    Add messages tests

--- test/messagestest/PostGet.scala
+++ test/messagestest/PostGet.scala
@@ -19,6 +19,123 @@ object PostGet extends PlaySpecification with JSON {
    */
   "POST /api/v0.1/user/room/messages " +
     """{ "users": [ "a", "b" ] } """.stripMargin +
+    "must be 401 Unauthorized Bad credentials" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withJsonBody(Json.parse(
+        """{ "users": [ "a", "b" ] } """.stripMargin))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Requires authentication"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 401
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b" ] } """.stripMargin +
+    "must be 400 Bad request Expecting text/json or application/json body" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withHeaders(("Authorization", "Basic YToxMjM0NTY3OA=="))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Expecting text/json or application/json body"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 400
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b" ] } """.stripMargin +
+    "must be 401 Unauthorized Bad credentials" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withHeaders(("Authorization", "Basic ejoxMjM0NTY3OA=="))
+        .withJsonBody(Json.parse(
+        """{ "users": [ "a", "b" ] } """.stripMargin))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Bad credentials"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 401
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b" ] } """.stripMargin +
+    "must be 401 Unauthorized Bad credentials" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withHeaders(("Authorization", "Basic YToxMjM0"))
+        .withJsonBody(Json.parse(
+        """{ "users": [ "a", "b" ] } """.stripMargin))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Bad credentials"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 401
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b", "c" ] } """.stripMargin +
+    "must be 404 Not found" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withHeaders(("Authorization", "Basic YToxMjM0NTY3OA=="))
+        .withJsonBody(Json.parse(
+        """{ "users": [ "a", "b", "c" ] } """.stripMargin))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Not found"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 404
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b", "c", "d" ] } """.stripMargin +
+    "must be 404 Not found" in {
+    running(FakeApplication()) {
+      val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
+        .withHeaders(("Authorization", "Basic YToxMjM0NTY3OA=="))
+        .withJsonBody(Json.parse(
+        """{ "users": [ "a", "b", "c", "d" ] } """.stripMargin))
+      val response = route(request)
+      Thread.sleep(5000)
+      response.isDefined mustEqual true
+      val result = Await.result(response.get, timeout)
+      val expectedResponse = Json.obj("messages" ->
+        Json.arr("Not found"))
+
+      contentAsString(response.get) mustEqual prettify(expectedResponse)
+      result.header.status mustEqual 404
+    }
+  }
+
+  "POST /api/v0.1/user/room/messages " +
+    """{ "users": [ "a", "b" ] } """.stripMargin +
     "must be 200 Ok" in {
     running(FakeApplication()) {
       val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
@@ -39,7 +156,7 @@ object PostGet extends PlaySpecification with JSON {
     "must be 200 Ok" in {
     running(FakeApplication()) {
       val request = FakeRequest(POST, "/api/v0.1/user/room/messages")
-        .withHeaders(("Authorization", "Basic YToxMjM0NTY3OA=="))
+        .withHeaders(("Authorization", "Basic YjoxMjM0NTY3OA=="))
         .withJsonBody(Json.parse(
         """{ "users": [ "a", "b" ] } """.stripMargin))
       val response = route(request)
